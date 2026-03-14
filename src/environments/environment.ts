4a8883c91e7104a787e69722a1c88222a5d54c78
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-03-13 20:15:04 -0400

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api',
-  mediaUrl: 'http://localhost:3000/api/uploads/',
+  // baseUrl: 'http://localhost:3000/api',
+  // mediaUrl: 'http://localhost:3000/api/uploads/',
   // soketServer : 'http://localhost:3000',
   //remoto vercel
-  // baseUrl: 'https://back-ecomm-mall.onrender.com/api',
-  // mediaUrl: 'https://back-ecomm-mall.onrender.com/api/uploads/',
+  baseUrl: 'https://back-ecomm-mall.onrender.com/api',
+  mediaUrl: 'https://back-ecomm-mall.onrender.com/api/uploads/',
   soketServer : 'https://back-ecomm-mall.onrender.com/',
-  // nombreSelected: 'Strapizza',
-  nombreSelected: 'Slidedish',
+  nombreSelected: 'Strapizza',
+  // nombreSelected: 'Slidedish',
   
   mediaUrlRemoto: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741218430/mallConnect',
   
