db2eb33ec5c9fd1e1e5680e6d3b45a6e507bd0b8
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-06-05 17:32:57 -0400

    fix enviar mensaje whatsapp1

--- src/app/pages/review-order/review-order.component.ts
+++ src/app/pages/review-order/review-order.component.ts
@@ -342,7 +342,7 @@ getWhatsAppMessage(): string {
   const tipoEntrega = formValues.tipoEntrega || 'No especificado';
   const telefono = formValues.telefono || this.identity.telefono || 'No registrado';
 
-  let message = `*Nuevo Pedido desde App Menu #${this.randomNum}*\n\n`;
+  let message = `*Nuevo Pedido desde App Zlipmenu #${this.randomNum}*\n\n`;
   message += `*Cliente:* ${firstName}\n`;
   message += `*Tipo Entrega:* ${tipoEntrega}\n`;
   message += `*Teléfono:* ${telefono}\n\n`;
